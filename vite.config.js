6c829174755b960172eb838b61be72a2780e70de
KimShota <sm11745@nyu.edu>
2025-12-07 02:02:45 +0400

    Changed vite file

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/astro-portfolio-view/',
-  root: resolve(__dirname, '.'),
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-    },
-  },
+      input: 'index.html',   // simple path, NOT absolute
+    }
+  }
 });
-
